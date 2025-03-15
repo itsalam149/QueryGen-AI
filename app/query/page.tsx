26c33473b420c8619069a152e7839eec60672251
JUNAID <151039989+zunxii@users.noreply.github.com>
2025-03-15 18:05:21 +0530

    Deployment issues

--- app/query/page.tsx
+++ app/query/page.tsx
@@ -1,21 +1,26 @@
 "use client";
+export const dynamic = "force-dynamic";
 import { useSearchParams } from "next/navigation";
 import Navbar from "@/components/Navbar";
 import { Button } from "@/components/ui/button";
 import { FaArrowLeft, FaCopy, FaCheck } from "react-icons/fa";
 import { useRouter } from "next/navigation";
-import { Suspense, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 export default function QueryResult() {
   const searchParams = useSearchParams();
-  const sqlQuery = searchParams.get("sql") || "No query generated";
   const router = useRouter();
+  const [sqlQuery, setSqlQuery] = useState<string>("Loading...");
   const [copied, setCopied] = useState(false);
+
+  useEffect(() => {
+    setSqlQuery(searchParams.get("sql") || "No query generated");
+  }, [searchParams]);
 
   const handleCopy = () => {
     navigator.clipboard.writeText(sqlQuery);
     setCopied(true);
-    setTimeout(() => setCopied(false), 2000); 
+    setTimeout(() => setCopied(false), 2000);
   };
 
   return (
@@ -23,7 +28,7 @@ export default function QueryResult() {
       <Navbar />
       <div className="p-6 max-w-2xl text-center">
         <h1 className="text-4xl font-extrabold mb-6">Generated SQL</h1>
-        
+
         <div className="relative bg-gray-800 p-12 rounded-lg text-sm font-mono overflow-auto w-full">
         <Suspense fallback={<p>Loading SQL Query...</p>}>
           <pre className="text-left">{sqlQuery}</pre>
@@ -35,7 +40,7 @@ export default function QueryResult() {
             {copied ? <FaCheck className="text-green-400" /> : <FaCopy />}
           </button>
         </div>
-        
+
         <Button
           className="mt-6 bg-purple-600 hover:bg-purple-700 flex items-center"
           onClick={() => router.push("/create")}
